5ec2bb1ad61b9cbcae03a4e7c383bf5d62dcf3d4
yn <jenny783113@gmail.com>
2025-04-25 15:50:08 +0800

    講解常數與變數

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+define("PI",3.1415926);
+
+$age=24;
+$name="詹雅年";
+$ename="yn";
+
+echo '我的年齡是：' . $age . '歲';
+echo "<br>";
+echo '我的名字是：' . $name;
+echo "<br>";
+echo PI;
+
+echo "<hr>";
+
+// define("PI",6.14159); >常數不能改
+$age=36;
+$name="王小明";
+
+echo '我的年齡是：' . $age . '歲';
+echo "<br>";
+echo '我的名字是：' . $name;
+echo "<br>";
+echo PI;
+
