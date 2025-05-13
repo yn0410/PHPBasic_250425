a1fe6c184916cd3358beef406dce3678a541b4da
yn <jenny783113@gmail.com>
2025-05-13 10:46:07 +0800

    L4字串 練習2

--- string/index.php
+++ string/index.php
@@ -45,12 +45,20 @@
     ?>
     <p>&nbsp;</p>
 
-    
+
     <hr>
     <h2>練習-2 字串分割</h2>
     <p>將”this,is,a,book”依”,”切割後成為陣列</p>
     <?php
+    $str2 = "this,is,a,book";
+    
+    $str2 = explode(",", $str2);
 
+    print_r($str2);
+    echo "<br>";
+    echo $str2[0];
+    echo "<br>";
+    // echo $str2; //echo 不能輸出陣列
     ?>
     <p>&nbsp;</p>
 
@@ -59,28 +67,46 @@
     <h2>練習-3 字串組合</h2>
     <p>將上例陣列重新組合成“this is a book”</p>
     <?php
-
+    // $str3 = implode(" ", $str2);
+    $str3 = join(" ", $str2);
+    echo $str3;
     ?>
     <p>&nbsp;</p>
 
 
     <hr>
     <h2>練習-4 子字串取用</h2>
-    <p>將” The reason why a great man is great is that he resolves to be a great man”只取前十字成為” The reason…”</p>
+    <p>將” The reason why a great man is great is that he resolves to be a great man”只取前十字(元)成為” The reason…”</p>
     <?php
+    $str4 = "The reason why a great man is great is that he resolves to be a great man";
+    $str4 = substr($str4, 0, 10) . "...";
 
+    // 老師做法
+    // $str4 = mb_substr($str4, 0, 10, "utf-8") . "...";
+
+
+    echo $str4;
     ?>
     <p>&nbsp;</p>
 
 
     <hr>
     <h2>練習-5 尋找字串與HTML、css整合應用</h2>
-    <p>給定一個句子，將指定的關鍵字放大</p>
-    <p>“學會PHP網頁程式設計，薪水會加倍，工作會好找”</p>
-    <p>請將上句中的 “程式設計” 放大字型或變色.</p>
+    <p>1.給定一個句子，將指定的關鍵字放大</p>
+    <p>2.“學會PHP網頁程式設計，薪水會加倍，工作會好找”</p>
+    <p>3.請將上句中的 “程式設計” 放大字型或變色.</p>
+    <!-- “學會PHP網頁<span style="font-size: 28px; color: blue;">程式設計</span>，薪水會加倍，工作會好找” -->
+    
     <?php
+    $str5 = "學會PHP網頁程式設計，薪水會加倍，工作會好找";
+    $keyword = "程式設計";
+    $style = "font-size: 28px; color: blue;";
 
+    $str5 = str_replace("$keyword", "<span style = '$style'>$keyword</span>", $str5);
+
+    echo $str5;
     ?>
+
 
     <p>&nbsp;</p>
     <p>&nbsp;</p>
